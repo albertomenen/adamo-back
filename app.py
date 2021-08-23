b9b4193cb6567a3eff91f753772d40dd9a2a3da5
Alf <alfonsocuestaalcantara@gmail.com>
2021-08-23 16:54:25 +0200

    cors

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import os
 from flask_migrate import Migrate
 from src import create_app, db
-from src import models
 from src.controllers import register_controllers
+from flask_cors import CORS
 
 
 app = create_app(os.environ.get('FLASK_ENV', 'dev'))
 migrate = Migrate(app, db)
 register_controllers(app)
-
-
-# from src.models import Role
-# ro = Role(**{'role_code': 'cosa', 'role_name': 'cosa'})
-# app.logger.info(ro)
+CORS(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0')
