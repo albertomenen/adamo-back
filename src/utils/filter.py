39667a5044d6cd49bf4f1f97459769fa999722fc
Alf <alfonsocuestaalcantara@gmail.com>
2021-09-07 16:25:27 +0200

    filters

--- src/utils/filter.py
+++ src/utils/filter.py
@@ -2,15 +2,15 @@ def filtering(data_list, filters):
     for filtr in filters:
         op = filtr.get('op')
         if op == 'equal':
-            data_list = [d for d in data_list if d.__dict__[filtr.get('field')] == filtr.get('value')]
+            data_list = [d for d in data_list if str(d.__dict__[filtr.get('field')]).lower() == str(filtr.get('value')).lower()]
         elif op == '>':
-            data_list = [d for d in data_list if d.__dict__[filtr.get('field')] > int(filtr.get('value'))]
+            data_list = [d for d in data_list if int(d.__dict__[filtr.get('field')]) > int(filtr.get('value'))]
         elif op == '<':
-            data_list = [d for d in data_list if d.__dict__[filtr.get('field')] < int(filtr.get('value'))]
+            data_list = [d for d in data_list if int(d.__dict__[filtr.get('field')]) < int(filtr.get('value'))]
         elif op == '>=':
-            data_list = [d for d in data_list if d.__dict__[filtr.get('field')] >= int(filtr.get('value'))]
+            data_list = [d for d in data_list if int(d.__dict__[filtr.get('field')]) >= int(filtr.get('value'))]
         elif op == '<=':
-            data_list = [d for d in data_list if d.__dict__[filtr.get('field')] <= int(filtr.get('value'))]
+            data_list = [d for d in data_list if int(d.__dict__[filtr.get('field')]) <= int(filtr.get('value'))]
         elif op == 'contains':
-            data_list = [d for d in data_list if filtr.get('value') in d.__dict__[filtr.get('field')]]
+            data_list = [d for d in data_list if str(filtr.get('value')).lower() in str(d.__dict__[filtr.get('field')]).lower()]
     return data_list
